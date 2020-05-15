539a42912136f9e31732da373d81649dfd06c95b
Sehej Jain <sehejjain@outlook.com>
2020-05-15 12:01:06 +0530

    Added Scraping and Scoring Scripts

---
+++ getScore.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_colwidth', 200)
+
+
+#NLP packages
+from textblob import TextBlob
+
+from preprocessing import process_likes
+from scraping import scrape
+
+
+def getScore(link):
+    vidName = scrape(link)
+    process_likes(vidName)
+    score = 0
+    video_comm = pd.read_csv(vidName)
+    video_comm = video_comm.drop(columns=['Unnamed: 0', 'Unnamed: 0.1'])
+    video_comm.rename(columns={'0': 'comment', '1': 'likes'}, inplace=True)
+    print(video_comm['likes'])
+    num_likes = video_comm['likes'].sum()
+    num_comments = video_comm['comment'].count()
+    video_comm['likes'] = video_comm['likes'] + 1
+    for index, comm in video_comm.iterrows():
+        try:
+            pol = TextBlob(comm['comment']).sentiment
+            score_comm = pol.polarity * comm['likes']
+            score += score_comm
+        except:
+            continue
+    return score/num_likes
+
+#Add links here
+print(getScore('https://www.youtube.com/watch?v=ZUkli9zgP_I'))
+# print(getScore('https://www.youtube.com/watch?v=fP17mIEv8lo'))
+# print(getScore('https://www.youtube.com/watch?v=QD0IM5tfnVQ'))
+# print(getScore('https://www.youtube.com/watch?v=rmQMKowvYeo'))
+# print(getScore('https://www.youtube.com/watch?v=3ScAXwImyw0'))
---
+++ scraping.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+import time
+import re
+
+
+def isEnglish(s):
+    try:
+        s.encode('ascii')
+    except UnicodeEncodeError:
+        return False
+    else:
+        return True
+
+
+def scrape(link):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(link)
+    driver.maximize_window()
+    time.sleep(5)
+    title = driver.find_element_by_xpath('//*[@id="container"]/h1/yt-formatted-string').text
+    print("Video Title: " + title)
+    print(
+        "-------------------------------------------------------------------------------------------------------------------")
+
+    comment_section = driver.find_element_by_xpath('//*[@id="comments"]')
+    driver.execute_script("arguments[0].scrollIntoView();", comment_section)
+    time.sleep(7)
+
+    last_height = driver.execute_script("return document.documentElement.scrollHeight")
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(3)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.documentElement.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"
+                               u"\U0001F300-\U0001F5FF"
+                               u"\U0001F680-\U0001F6FF"
+                               u"\U0001F1E0-\U0001F1FF"
+                               "]+", flags=re.UNICODE)
+
+    name_elems = driver.find_elements_by_xpath('//*[@id="author-text"]')
+    comment_elems = driver.find_elements_by_xpath('//*[@id="content-text"]')
+    comment_like_elems = driver.find_elements_by_class_name('style-scope ytd-comment-action-buttons-renderer')
+    num_of_names = len(name_elems)
+    comments = []
+    for i in range(num_of_names):
+        username = name_elems[i].text  # .replace(",", "|")
+        # username = emoji_pattern.sub(r'', username)
+        # username = str(username).replace("\n", "---")
+        comment = comment_elems[i].text
+        comment_likes = comment_like_elems[i].text
+        # .replace(",", "|")
+        # comment = emoji_pattern.sub(r'', comment)
+        # comment = str(comment).replace("\n", "---")
+        comments.append([comment, comment_likes])
+        # if isEnglish(comment) == False:
+        #   comment = "NOT ENGLISH"
+
+#    print(comments)
+    import pandas as pd
+    df = pd.DataFrame(comments)
+    df.to_csv('csvs/' + title + 'comm.csv')
+    #return file name
+    return 'csvs/' + title + 'comm.csv'
+    driver.close()
+
